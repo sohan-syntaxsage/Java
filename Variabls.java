dc23a955ac6be16bfe524798ddf38248816e044e
Md. Sohan <mdsohan17.178@gmail.com>
2025-09-15 10:52:07 +0600

    Delete Variabls.java

--- Variabls.java
+++
@@ -1,8 +0,0 @@
-public class Variabls {
-    public static void main (String[] args){
-        int number= 24;
-        System.out.println(number);
-        number = 20;
-        System.out.println(number);
-    }
-}
